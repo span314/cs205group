b5ff078c6be9b279cb4f649f7e6d7455f5559ce8
Andrew Ross <aross@holygpu01.rc.fas.harvard.edu>
2017-03-23 17:26:54 -0400

    benchmarking and questionable parallezation for BFS

--- BFS.h
+++ BFS.h
@@ -1,2 +1,5 @@
 int edgelist_BFS(const int* edges, const int* offsets, int start, int goal, int N);
 void edgelist_matrix_vector_multiply(const int* edges, const int* offsets, int* vector, int* result, int N);
+
+int edgelist_BFS_parallel(const int* edges, const int* offsets, int start, int goal, int N);
+void edgelist_matrix_vector_multiply_parallel(const int* edges, const int* offsets, int* vector, int* result, int N);
--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ endif
 
 unit_tests: unit_tests.o graph.o APSP.o BFS.o
 bench_apsp: bench_apsp.o graph.o APSP.o
+bench_bfs: bench_bfs.o graph.o BFS.o
 bfs: BFS.o graph.o
 
 clean:
